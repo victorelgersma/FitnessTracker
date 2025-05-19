47176cf71c3807a060f12888cdb6ed0e6a7a200a
Victor Elgersma <victor.elgersma41@thg.com>
2025-05-19 20:41:08 +0100

    pullups added

---
+++ helpers.js
@@ -0,0 +1,9 @@
+
+     // Utility to get today's date in YYYY-MM-DD format
+       function getTodayISO() {
+            const today = new Date();
+            const year = today.getFullYear();
+            const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+            const day = String(today.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        }
---
+++ readme.md
@@ -0,0 +1,9 @@
+
+## Technologies used
+
+- HTML
+- Tailwind
+- 📀 The [localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage) API
+
+# How does it work?
+
