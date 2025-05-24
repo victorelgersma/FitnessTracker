b88e48440ab9fb66a1d7de480501f5388e8afdf5
Victor Elgersma <victor.elgersma41@thg.com>
2025-05-24 23:10:31 +0100

    added index to templates

--- build.js
+++ build.js
@@ -2,11 +2,15 @@ const ejs = require('ejs');
 const fs = require('fs');
 
 const configs = [
-  { filename: 'pushups.html', title: 'Push-up', storageKey: 'pushupLogData', unitKey: 'pushups', unitLabel: 'push-ups' },
-  { filename: 'pullups.html', title: 'Pull-up', storageKey: 'pullupLogData', unitKey: 'pullups', unitLabel: 'pull-ups' },
-  { filename: 'swimming.html', title: 'Swim', storageKey: 'swimLogData', unitKey: 'meters', unitLabel: 'meters' },
-  { filename: 'running.html', title: 'Run', storageKey: 'runLogData', unitKey: 'km', unitLabel: 'km' }
+  { filename: 'pushups.html', title: 'Push-up', storageKey: 'pushupLogData', unitKey: 'pushups', unitLabel: 'push-ups', color: 'lime-400' },
+  { filename: 'pullups.html', title: 'Pull-up', storageKey: 'pullupLogData', unitKey: 'pullups', unitLabel: 'pull-ups', color: 'blue-400' },
+  { filename: 'swimming.html', title: 'Swim', storageKey: 'swimLogData', unitKey: 'meters', unitLabel: 'meters', color: 'teal-400' },
+  { filename: 'running.html', title: 'Run', storageKey: 'runLogData', unitKey: 'km', unitLabel: 'km', color: 'orange-400' }
 ];
+
+const indexTemplate = fs.readFileSync('index-template.ejs', 'utf8');
+const indexHtml = ejs.render(indexTemplate, { configs });
+fs.writeFileSync('public/index.html', indexHtml);
 
 const template = fs.readFileSync('logger-template.ejs', 'utf8');
 
